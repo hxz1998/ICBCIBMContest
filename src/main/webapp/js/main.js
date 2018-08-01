d3d1d8115727c1a309f12668107a5bbb1afbcbb9
Monkey <1466947023@qq.com>
2018-08-01 09:17:06 +0800

    分离组件完成。存在问题：Vue如何调用bootstrap模态框显示问题。

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,6 +1,84 @@
 
 Vue.http.options.emulateJSON = true;
 Vue.http.options.crossOrigin = true;
+Vue.component('qr-image-dialog', {
+    data: function () {
+        return {
+            imageSrc: '',
+            message: ''
+        }
+    },
+    template: '#qr-image-dialog',
+    methods: {
+        toggle: function () {
+            $('#qr_image_modal').modal('toggle');
+        }
+    }
+});
+
+Vue.component('qr-pay-panel', {
+    data: function () {
+        return {
+            merId: '',
+            storeCode: '',
+            outTradeNo: '',
+            orderAmt: '',
+            tradeDate: '',
+            tradeTime: '',
+            payExpire: '',
+            tporderCreateIp: '',
+            notifyFlagInput: '',
+        }
+    },
+    template: '#qrPay',
+    methods: {
+        postDefault: function () {
+            this.merId = '020002040095';
+            this.storeCode = '02000015087';
+            this.outTradeNo = 'ZHL777O15002039';
+            this.orderAmt = '7370';
+            this.tradeDate = '20171210';
+            this.tradeTime = '160346';
+            this.payExpire = '1200';
+            this.tporderCreateIp = '127.0.0.1';
+            this.notifyFlagInput = '1';
+            this.post();
+        },
+        post: function () {
+            Pace.restart();
+            this.$http.post('http://localhost:8080/demo/qr/getQrCode', {
+                'merId': this.merId,
+                'storeCode': this.storeCode,
+                'outTradeNo': this.outTradeNo,
+                'orderAmt': this.orderAmt,
+                'tradeDate': this.tradeDate,
+                'tradeTime': this.tradeTime,
+                'payExpire': this.payExpire,
+                'tporderCreateIp': this.tporderCreateIp,
+                'notifyFlag': this.notifyFlagInput
+            }, {
+                'headers': {
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }).then(function (response) {
+                var respText = response.bodyText;
+                var respJson = JSON.parse(respText);
+                if (respJson.status === "0") {
+                    this.$refs.qr_image_dialog.imageSrc = respJson.data;
+                    this.$refs.qr_image_dialog.message = '';
+                    console.log(respJson)
+                } else {
+                    this.$refs.qr_image_dialog.imageSrc = '';
+                    this.$refs.qr_image_dialog.message = respJson.msg;
+                }
+                this.$refs.qr_image_dialog.toggle();
+            }, function (response) {
+                console.log(response)
+            });
+        }
+    }
+
+});
 
 var routes = [
     {
@@ -29,17 +107,6 @@ var router = new VueRouter({
 
 var vm = new Vue({
     el: '#app',
-    router: router,
-    mounted: function () {
-        console.log(router)
-    }
+    router: router
 });
 
-
-var qr_image_dialog = new Vue({
-    el: '#qr-image-dialog',
-    data: {
-        imageSrc: '',
-        message: ''
-    },
-});
